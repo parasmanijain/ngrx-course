bb092d5bc111e42f0c4844d776490e5fb2883c96
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-11 11:41:36 +0530

    code updated

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,22 +1,17 @@
-import {createAction, props} from '@ngrx/store';
-import {Course} from './model/course';
-import {Update} from '@ngrx/entity';
-import {UpdateStr} from '@ngrx/entity/src/models';
-
+import { createAction, props } from "@ngrx/store";
+import { Course } from "./model/course";
+import { Update } from "@ngrx/entity";
 
 export const loadAllCourses = createAction(
-    "[Courses Resolver] Load All Courses"
+  "[Courses Resolver] Load All Courses"
 );
-
 
 export const allCoursesLoaded = createAction(
-    "[Load Courses Effect] All Courses Loaded",
-    props<{courses: Course[]}>()
+  "[Load Courses Effect] All Courses Loaded",
+  props<{ courses: Course[] }>()
 );
-
 
 export const courseUpdated = createAction(
   "[Edit Course Dialog] Course Updated",
-  props<{update: Update<Course>}>()
+  props<{ update: Update<Course> }>()
 );
-
