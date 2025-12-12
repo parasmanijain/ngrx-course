fc2018cb6050d97537b3b296eb0bd40e6a7606b3
Parasmani Jain <parasmani.jain2208@gmail.com>
2025-12-12 12:49:40 +0530

    code updated

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,6 @@
 import {
   ActivatedRouteSnapshot,
+  CanActivate,
   Router,
   RouterStateSnapshot,
 } from "@angular/router";
@@ -11,7 +12,7 @@ import { AppState } from "../reducers";
 import { isLoggedIn } from "./auth.selectors";
 
 @Injectable()
-export class AuthGuard {
+export class AuthGuard implements CanActivate {
   constructor(private store: Store<AppState>, private router: Router) {}
 
   canActivate(
